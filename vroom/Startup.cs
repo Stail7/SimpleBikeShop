6ff65c237b9ced455921e0e387a433220c620563
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-01-29 09:29:30 +0200

    Deleted unnecessary files

--- vroom/Startup.cs
+++ vroom/Startup.cs
@@ -59,7 +59,7 @@ namespace vroom
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Bike/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
